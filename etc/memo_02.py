4694a863abd9c46dda3364da0f15a88495de0ef9
YoungriKIM <lemontleo0311@gmail.com>
2021-05-18 01:10:47 +0900

    2021-05-18

--- etc/memo_02.py
+++ etc/memo_02.py
@@ -1,33 +1,36 @@
-# 소스 코드
-# 재귀적 구현!!
+# n, c = 5, 3
+# house = [1,2,8,4,9]
 
-# n, target = 10, 7
-# array = [1,3,5,7,9,11,13,15,17,19]
 
-# 입력 받기
-n, target = map(int, input().split())
-array = list(map(int, input().split()))
+n,c = map(int,input().split())
+house = []
+for _ in range(n):
+    x = int(input())
+    house.append(x)
 
-# 함수 정의
-def binary_search(array, target, start, end):
-    # 스타트가 엔드보다 크다면 none 반환
-    if start > end:
-        return None
-    # mid 지정
-    mid = (start + end) // 2
-    # 미드가 타겟과 같다면 미드값 반환
-    if array[mid] == target:
-        return mid
-    # 미드가 타겟보다 크면 타켓이 미드 전에 있다는 거니까
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid-1)
-    # 혹인 미드가 타겟보다 작으면 미드 후에 있다는 거니까
+house.sort()
+
+# 좌표값의 최소값
+start = 1
+# 가장 높은 좌표와 가장 낮은 좌표의 차이
+end = house[-1] - house[0]
+
+result = 0
+
+while (start <= end):
+    mid = (start+end)//2 # 해당 gap
+    old = house[0]
+    count = 1
+
+    for i in range(1, len(house)):
+        if house[i] >= old+mid: # gap 이상
+            count+=1
+            old = house[i]
+    
+    if count >=c:
+        start = mid + 1
+        result = mid
     else:
-        return binary_search(array, target, mid+1, end)
+        end = mid - 1
 
-# 함수 적용
-result = binary_search(array, target, 0, n-1)
-if result == None:
-    print('원소가 존재하지 않습니다.')
-else:
-    print(result+1)
+print(result)
