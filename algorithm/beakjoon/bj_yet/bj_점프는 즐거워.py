5536e8b0b5c7173271d0e37df9d2c5f098e31aad
YoungriKIM <lemontleo0311@gmail.com>
2021-12-01 22:40:41 +0900

    bj

--- algorithm/beakjoon/bj_yet/bj_점프는 즐거워.py
+++ algorithm/beakjoon/bj_yet/bj_점프는 즐거워.py
@@ -1,10 +1,11 @@
 import sys
 data = list(sys.stdin.readline().split())
-for sample in data:
-    n = int(sample[0]);sample = sample[1:]
+
+for a_l in data:
+    n = int(a_l[0]);a_l = a_l[1:]
     if n == 1 : print('Jolly')
     elif n > 1 : 
-        r_l = list(set([abs(int(sample[i])-int(sample[i+1])) for i in range(len(sample)-1) if int(sample[i]) != int(sample[i+1])]))
+        r_l = list(set([abs(int(a_l[i])-int(a_l[i+1])) for i in range(len(a_l)-1) if int(a_l[i]) != int(a_l[i+1])]))
         if r_l == list(range(1, n)) : print('Jolly')
         else: print('Not jolly')
     else: print('Not jolly')
